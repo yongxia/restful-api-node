36647af4e04eee4707f9a935df8569cc863224bd
Yong Xia <xywork008@gmail.com>
2019-01-26 17:48:56 -0600

    add user sign up and email address validation

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const productRounter = require('./api/routes/products');
-const orderRounter = require('./api/routes/orders');
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/users');
 
 mongoose.connect('mongodb://root:' + process.env.MONGO_ALLAS_PW +
     '@cluster0-shard-00-00-qfv4f.mongodb.net:27017,cluster0-shard-00-01-qfv4f.mongodb.net:27017,cluster0-shard-00-02-qfv4f.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
@@ -28,8 +29,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/products', productRounter);
-app.use('/orders', orderRounter);
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+app.use('/users', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
